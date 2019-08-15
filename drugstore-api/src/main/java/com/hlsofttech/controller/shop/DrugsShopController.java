31b4e7f7169c4aa72e0eb6a60ba592d55d6e8796
suntengfei <791471021@qq.com>
2019-08-15 17:11:31 +0800

    中台-店铺接口

---
+++ drugstore-api/src/main/java/com/hlsofttech/controller/shop/DrugsShopController.java
@@ -0,0 +1,100 @@
+package com.hlsofttech.controller.shop;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.bjucloud.common.DataGrid;
+import com.bjucloud.common.ExecuteResult;
+import com.bjucloud.common.Pager;
+import com.bjucloud.storecenter.dto.ShopCategorySellerDTO;
+import com.bjucloud.storecenter.dto.ShopDTO;
+import com.bjucloud.storecenter.service.ShopCategorySellerExportService;
+import com.bjucloud.storecenter.service.ShopExportService;
+import com.hlsofttech.annotation.AuthPower;
+import com.hlsofttech.common.Constant;
+import com.hlsofttech.rsp.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ * @Author: suntf
+ * @Description:药店相关接口
+ * @Date: 2019/8/15
+ **/
+@RestController
+@Api(tags = "用户端-店铺", value = "用户端-店铺", description = "用户端-店铺 @author suntf")
+public class DrugsShopController {
+
+    @Reference(version = Constant.VERSION_ZT, timeout = Constant.TIMEOUT)
+    ShopExportService shopExportService;
+
+    @Reference(version = Constant.VERSION_ZT, timeout = Constant.TIMEOUT)
+    ShopCategorySellerExportService shopCategorySellerExportService;
+
+    /***
+     * @Author: suntf
+     * @Description:药店列表
+     * @Date: 2019/8/15
+     * @return: com.hlsofttech.rsp.Result
+     **/
+    @AuthPower(avoidVersion = false, avoidPower = true, avoidSign = true, avoidLogin = false, avoidPlatform = true)
+    @ApiOperation(value = "药店列表", notes = "药店列表", httpMethod = "GET")
+    @GetMapping("/api/drugsShop/list")
+    public Result queryShopList() {
+
+        ShopDTO shopDTO = new ShopDTO();
+        shopDTO.setShopType(10);
+        Pager<ShopDTO> pager = new Pager<>(1, 15);
+        ExecuteResult<DataGrid<ShopDTO>> result = shopExportService.findShopInfoByCondition(shopDTO, pager);
+        if (result.isSuccess() && result.getResult() != null) {
+            DataGrid<ShopDTO> data = result.getResult();
+            return Result.newSuccessResult(data);
+        } else {
+            return null;
+        }
+    }
+//
+//    /***
+//     * @Author: suntf
+//     * @Description:根据id查询药店详细信息
+//     * @Date: 2019/8/15
+//     * @return: com.hlsofttech.rsp.Result
+//     **/
+//    @AuthPower(avoidVersion = false, avoidPower = true, avoidSign = true, avoidLogin = false, avoidPlatform = true)
+//    @ApiOperation(value = "药店详情", notes = "药店详情", httpMethod = "GET")
+//    @GetMapping("/api/drugsShop/info")
+//    public Result queryShopInfo(Long shopId) {
+//
+//        ExecuteResult<ShopDTO> result = shopExportService.findShopInfoById(shopId);
+//        if (result.isSuccess() && result.getResult() != null) {
+//            return Result.newSuccessResult(result.getResult());
+//        } else {
+//            return null;
+//        }
+//    }
+
+
+    /***
+     * @Author: suntf
+     * @Description:根据店铺id查询店铺内分类列表
+     * @Date: 2019/8/15
+     * @return: com.hlsofttech.rsp.Result
+     **/
+    @AuthPower(avoidVersion = false, avoidPower = true, avoidSign = true, avoidLogin = false, avoidPlatform = true)
+    @ApiOperation(value = "店铺内分类列表", notes = "店铺内分类列表", httpMethod = "GET")
+    @GetMapping("/api/drugsShop/category/list")
+    public Result queryShopCategoryList(Long shopId, int lev) {
+        ShopCategorySellerDTO shopCategorySellerDTO = new ShopCategorySellerDTO();
+        shopCategorySellerDTO.setShopId(shopId);
+        shopCategorySellerDTO.setLev(lev);
+
+        Pager<ShopCategorySellerDTO> pager = new Pager<>(1, 99);
+        ExecuteResult<DataGrid<ShopCategorySellerDTO>> result = shopCategorySellerExportService.queryShopCategoryList(shopCategorySellerDTO, pager);
+        if (result.isSuccess() && result.getResult() != null) {
+            return Result.newSuccessResult(result.getResult().getRows());
+        } else {
+            return null;
+        }
+    }
+
+}
